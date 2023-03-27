71cd43c0eb979b3f141ff190c1f534b8d786456d
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-03-27 11:55:17 +0530

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+require("dotenv").config()
 const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./Router/route');
@@ -10,9 +10,23 @@ app.use(cors());
 
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://new_user:jk1BBWwmxQpZ31zO@cluster0.pxvwsjp.mongodb.net/sharetext")
-.then(() => { console.log("MongoDB is Connected.....") })
-.catch ((error) => { console.log(error.message) })
+// mongoose.connect("mongodb+srv://new_user:jk1BBWwmxQpZ31zO@cluster0.pxvwsjp.mongodb.net/sharetext")
+// .then(() => { console.log("MongoDB is Connected.....") })
+// .catch ((error) => { console.log(error.message) })
+
+
+const DB = process.env.DATABASE
+
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(() => console.log("DataBase Connected"))
+    .catch((err) => { console.log(err) })
+
 
 
 app.get('/', (req, res) => {
