cdf6d97e035a54b274f84398565800945a8d337b
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-03-27 13:03:52 +0530

    env

--- app.js
+++ app.js
@@ -1,41 +1,22 @@
 require("dotenv").config()
 const express = require('express');
-const mongoose = require('mongoose');
-const route = require('./Router/route');
 const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+
+const route = require('./Router/route');
+
+require("./db/conn")
+
 const app = express();
 
+
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
-
 app.use(express.json());
-
-// mongoose.connect("mongodb+srv://new_user:jk1BBWwmxQpZ31zO@cluster0.pxvwsjp.mongodb.net/sharetext")
-// .then(() => { console.log("MongoDB is Connected.....") })
-// .catch ((error) => { console.log(error.message) })
-
-
-const DB = process.env.DATABASE
-
-
-// mongoose.set('strictQuery', true)
-
-mongoose.connect(DB, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-})
-    .then(() => console.log("DataBase Connected"))
-    .catch((err) => { console.log(err) })
+app.use(route);
 
 
 
-app.get('/', (req, res) => {
-    res.send("Working")
-})
-
-app.use('/', route);
-
-
-
-app.listen(PORT, () => { console.log(`Server Run on ${PORT} ...`)} );
-
+app.listen(PORT, () => {
+    console.log(`Server Run on ${PORT} ...`)
+});
