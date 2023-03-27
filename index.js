e8af8b59b585039a5fa63fb07325dbfea3cacebe
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-03-27 11:18:52 +0530

    done

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-require("dotenv").config()
+
 const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./Router/route');
 const cors = require('cors');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
 
-
-const DB = process.env.DATABASE
-
-mongoose.connect(DB)
+mongoose.connect("mongodb+srv://new_user:jk1BBWwmxQpZ31zO@cluster0.pxvwsjp.mongodb.net/sharetext")
 .then(() => { console.log("MongoDB is Connected.....") })
 .catch ((error) => { console.log(error.message) })
 
